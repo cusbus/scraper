ca926af2315e97e9f9c363e8009507eaeb09970f
Justin Caryk <cary9983@gmail.com>
2017-12-13 17:01:51 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express')
+var fs = require('fs')
+var request = require('request')
+var cheerio = require('cheerio')
+var app = express()
+
+app.get('/scrape', function (req, res) {
+
+    //target url
+    url = 'https://www.npr.org/'
+
+    var headline, urlSlug
+    var json = {
+        items: []
+    }
+
+    //managing our requests
+    request(url, function (error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html)
+
+            items = $('.story-text').toArray()
+
+            for (let i = 0; i < items.length; i++) {
+                let item = items[i]
+                let article = {}
+
+                article.headline = $('.title', item).first().text()
+                article.teaser = $('.teaser', item).first().text()
+                
+                if (article.teaser) {
+                    json.items.push(article)
+                }
+            }
+
+            console.log(json)
+
+            //writing to our file system
+            fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+                console.log('File written')
+            })
+
+        }
+    })
+
+    //response to complete the circle of life
+    res.send('check da console')
+
+})
+
+app.listen('8080')
+console.log('Magic happens on port 8080')
+
+exports = module.exports = app
