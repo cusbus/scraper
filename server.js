66ba3d337d5db7c5d17bb900456bc5fe93a5b43e
Justin Caryk <cary9983@gmail.com>
2017-12-13 18:24:22 -0800

    added a commented console app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var request = require('request')
 var cheerio = require('cheerio')
 var app = express()
 
+
 app.get('/scrape', function (req, res) {
 
     //target url
@@ -27,7 +28,7 @@ app.get('/scrape', function (req, res) {
 
                 article.headline = $('.title', item).first().text()
                 article.teaser = $('.teaser', item).first().text()
-                
+
                 if (article.teaser) {
                     json.items.push(article)
                 }
@@ -43,7 +44,7 @@ app.get('/scrape', function (req, res) {
         }
     })
 
-    //response to complete the circle of life
+    // response to complete the circle of life
     res.send('check da console')
 
 })
@@ -52,3 +53,56 @@ app.listen('8080')
 console.log('Magic happens on port 8080')
 
 exports = module.exports = app
+
+
+
+//======================== CONSOLE APP ============================ //
+
+// var express = require('express')
+// var fs = require('fs')
+// var request = require('request')
+// var cheerio = require('cheerio')
+// var app = express()
+
+//     //target url
+//     let url = 'https://www.npr.org/'
+
+//     var headline, urlSlug
+//     var json = {
+//         items: []
+//     }
+
+//     //managing our requests
+//     request(url, function (error, response, html) {
+//         if (!error) {
+//             var $ = cheerio.load(html)
+
+//             items = $('.story-text').toArray()
+
+//             for (let i = 0; i < items.length; i++) {
+//                 let item = items[i]
+//                 let article = {}
+
+//                 article.headline = $('.title', item).first().text()
+//                 article.teaser = $('.teaser', item).first().text()
+
+//                 if (article.teaser) {
+//                     json.items.push(article)
+//                 }
+//             }
+
+//             console.log(json)
+
+//             //writing to our file system
+//             fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+//                 console.log('File written')
+//             })
+
+//         }
+//     })
+
+
+// app.listen('8080')
+// console.log('Magic happens on port 8080')
+
+// exports = module.exports = app
